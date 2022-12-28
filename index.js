bb5c95f83cb5372b19a98b01820dbc74d3bf730a
prince-kumar1504 <prince15raja04@gmail.com>
2022-12-28 15:39:35 +0530

    dice-game

---
+++ index.js
@@ -0,0 +1,27 @@
+var randomNumber1 = Math.floor((Math.random()*6)+1);
+
+
+var randomimg1 = "images/dice"+randomNumber1+".png";
+
+
+document.querySelectorAll("img")[0].setAttribute("src",randomimg1);
+
+
+var randomNumber2 = Math.floor((Math.random()*6)+1);
+
+
+var randomimg2 = "images/dice"+randomNumber2+".png";
+
+
+document.querySelectorAll("img")[1].setAttribute("src",randomimg2);
+
+if(randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML ="🚩Player 1 wins";
+}
+else if(randomNumber1 < randomNumber2){
+    document.querySelector("h1").innerHTML ="Player 2 wins🚩";
+}
+else{
+    document.querySelector("h1").innerHTML ="DRAW!";
+}
+
